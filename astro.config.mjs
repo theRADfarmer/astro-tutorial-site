6de473cfbcdc61520bfa9860f40e9d6020e04176
theRADfarmer <theradfarmer@gmail.com>
2025-05-20 20:56:25 -0500

    feat: add theme toggle and greeting component; update styles and dependencies
    - Added ThemeIcon component for dark/light theme toggle functionality.
    - Introduced Greeting component to display random greetings.
    - Updated global styles to support dark theme.
    - Modified about page to set finished status to true.
    - Updated index page to include Greeting component.
    - Added Preact as a dependency and updated TypeScript configuration for JSX support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://roberts-astro-tutorial-site.netlify.app/"
+  site: "https://roberts-astro-tutorial-site.netlify.app/",
+  integrations: [preact()]
 });
